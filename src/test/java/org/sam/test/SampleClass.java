bd049300e6ee87c8efe55072482d7e4e476e7447
arjunkumar67 <arjunkumar666777@gmail.com>
2020-12-19 09:50:11 +0530

    updated

---
+++ src/test/java/org/sam/test/SampleClass.java
@@ -0,0 +1,11 @@
+package org.sam.test;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+
+	@Test
+	private void method() {
+		System.out.println("Method printed successfully");
+	}
+}
